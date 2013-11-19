43e7c2d1b9380605d9bc6b83c50a3bc2c5eda4be
Bachue Zhou <bachue.shu@gmail.com>
2013-11-19 13:25:27 +0800

    mock data and now current project will be switch after it was deleted

--- js/main.js
+++ js/main.js
@@ -40,18 +40,7 @@ var global = garmApp.controller('Global', function($scope) {
 
     // <Mock> TODO: Remove Mock
         $scope.controller_names = ['Exceptions'];
-        $scope.projects = [
-            {id: 1, name: 'BUS', percent: 12, subscriptions: [
-                {id: 1, email: 'bachue.shu@mail.com', interval_days: 1},
-                {id: 2, email: 'rong.zhou@mail.com', interval_days: 7}]
-            },
-            {id: 2, name: 'Njord', percent: 65, subscriptions: [
-                {id: 1, email: 'yuz@mail.com', interval_days: 7},
-                {id: 2, email: 'yu.zhou@mail.com', interval_days: 7}]},
-            {id: 3, name: 'Phoenix', percent: 92, subscriptions: [
-                {id: 1, email: 'zhour@company.com', interval_days: 1},
-                {id: 2, email: 'jinpingx@gov.cn', interval_days: 1}]}
-        ];
+        $.ajax({url: '/mock-data.json', async: false, dataType: 'json', success: function(data) { $scope.projects = data; }});
     // </Mock>
     $scope.avaiable_interval_days = [1, 3, 7];
     $scope.current_project = $scope.projects[0];
@@ -162,7 +151,7 @@ var global = garmApp.controller('Global', function($scope) {
         }
 
         $('#config-modal ul.nav li:eq(' + idx +')').remove();
-        
+
         var nextTab = _.last($('#config-modal ul.nav li:eq(' + idx +') a,#config-modal ul.nav li:eq(0) a'));
         if(nextTab) $(nextTab).tab('show');
         else $scope.add_project();
@@ -187,6 +176,11 @@ var global = garmApp.controller('Global', function($scope) {
 
     $scope.save_config = function() {
         $scope.config_saving_confirmed = true;
+
+        if($scope.projects.indexOf($scope.current_project) == -1) {
+            $scope.current_project = $scope.projects[0];
+        }
+
         //TODO: Send cmds to backend
         console.log($scope.config_change_commands);
     };
