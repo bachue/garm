e8f4dbc1af2b40613ad71bf8fd9144fa5ca1b734
Bachue Zhou <bachue.shu@gmail.com>
2013-11-30 23:52:05 +0800

    add model and validations

--- models.rb
+++ models.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+  has_many :summary_subscriptions
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
+end
+
+class SummarySubscription < ActiveRecord::Base
+  belongs_to :project
+  validates :project_id, :interval_days, presence: true
+  validates :email, presence: true, length: {maximum: 40}
 end
