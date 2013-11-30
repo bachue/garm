3a0fab536f7c4c1c1554e440d35073bf4feb23c6
Bachue Zhou <bachue.shu@gmail.com>
2013-11-30 22:50:40 +0800

    handling project commands, not finished

--- Gemfile
+++ Gemfile
@@ -1,11 +1,15 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'sinatra-activerecord'
+gem 'yajl-ruby'
+gem 'activesupport', require: 'active_support/all'
 
 group :development do
   gem 'rake'
   gem 'pry'
+  gem 'pry-byebug'
   gem 'sqlite3'
   gem 'shotgun', require: nil
   gem 'foreman', require: nil
--- app.rb
+++ app.rb
@@ -1,12 +1,44 @@
 #!/usr/bin/env ruby
+require 'sinatra/json'
+
 require_relative 'env'
+require_relative 'models'
 
 if $options.env.development?
   set :database, 'sqlite3:///db/development.sqlite3'
 end
 
 post '/projects/run_commands' do
-  params.inspect
+  return status 400 if params['commands'].blank?
+  commands = JSON.load(params['commands'])
+  new_project_list, new_subscription_list = {}, {}
+
+  Project.transaction do
+    commands.each do |command|
+      case command['cmd']
+      when 'add_project'
+        project = Project.new name: command['project_name']
+        if project.save
+          new_project_list[project.name] = project.id
+        else
+          status 400 and raise ActiveRecord::Rollback
+        end
+      when 'edit_project'
+        project = Project.find command['project_id'] rescue status 400 and raise ActiveRecord::Rollback
+        project.update name: command['project_name']
+        status 400 and raise ActiveRecord::Rollback unless project.save
+      when 'del_project'
+        project = Project.find command['project_id'] rescue status 400 and raise ActiveRecord::Rollback
+        status 400 and raise ActiveRecord::Rollback unless project.destroy
+      when 'add_subscription'
+      when 'edit_subscription'
+      when 'del_subscription'
+      else
+        status 400 and raise ActiveRecord::Rollback
+      end
+    end
+  end
+  json new_projects: new_project_list, new_subscriptions: new_subscription_list if status == 200
 end
 
 get '/js/env.js' do
