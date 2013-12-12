fc44538a0b8380ccf64526d1344cbe57c0cf46d1
Bachue Zhou <bachue.shu@gmail.com>
2013-12-12 13:15:51 +0800

    now we can really filter category by unresolved

--- public/js/exception_filters.js
+++ public/js/exception_filters.js
@@ -1,5 +1,5 @@
 define(['filters'], function(filters) {
-    filters.filter('orderCategoryBy', function(){
+    filters.filter('orderCategoryBy', function() {
         return function(items, field, reverse) {
             var sortfunc = null, factor = reverse ? -1 : 1;
             if (field == 'datetime') {
@@ -8,9 +8,23 @@ define(['filters'], function(filters) {
                 sortfunc = function(item) { return item.exception_size * factor; };
             } else if(field == 'frequence') {
                 sortfunc = function(item) { return item.frequence * factor; };
+            } else {
+                throw 'Cannot order by this field: ' + field;
             };
 
             return _.sortBy(items, sortfunc)
+        };
+    });
+
+    filters.filter('filterCategory', function() {
+        return function(items, scope) {
+            if (scope == 'All') {
+                return items;
+            } else if (scope == 'Unresolved Only') {
+                return _.filter(items, function(item) { return !item.resolved; })
+            } else {
+                throw 'Doesn\'t Support this scope: ' + scope;
+            };
         };
     });
 });
